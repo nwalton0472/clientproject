bfea6065c45c48df7fe6290fd2eb52767f4ca67c
Neota Walton <neota1951@gmail.com>
2019-05-16 22:20:59 +0000

    hello

--- js/script.js
+++ js/script.js
@@ -6,3 +6,37 @@ $('h1').click(function(){
     $('h1').css('color', 'green');
     $('h1').text('Ready to Code');
 });
+
+function getMusicData(searchTerm) {
+    var url= "https://api.soundcloud.com/tracks?q=beyonce&client_id=5aa8e389ba4e24b6106af5159ab3e344";
+    $.ajax({
+          url: url ,
+          method: "GET",
+          success: function(songs) {
+              var imageSrc;
+              var imageHtml;
+              
+              songs.forEach(function(song){
+                  console.log(song);
+              });
+              
+              
+            
+              $("body").append(imageHtml);
+          },
+        }); 
+}
+
+   
+   
+   
+    $('#searchButton').click(function() {
+        var searchTerm = $("#searchTerm").val();
+        getMusicData(searchTerm);
+        
+        
+        
+        
+        
+        
+    });
